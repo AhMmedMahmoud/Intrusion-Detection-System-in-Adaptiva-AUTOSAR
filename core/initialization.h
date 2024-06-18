711ec9943049677fe9b262295cdb51ed5365bde5
AhMmedMahmoud <ahmed.mahmoud2322002@gmail.com>
2024-06-19 01:44:33 +0300

    core

---
+++ core/initialization.h
@@ -0,0 +1,14 @@
+#ifndef INITIALIZATION_H
+#define INITIALIZATION_H
+
+#include "result.h"
+
+namespace ara
+{
+    namespace core
+    {
+        ara::core::Result<void> Initialize () noexcept;
+    }
+}
+
+#endif
